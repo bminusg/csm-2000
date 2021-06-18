ed9c823957913a7912cae045ad636919fc261583
Benny Gebauer <info@bminusg.de>
2021-06-18 10:22:40 +0200

    Adding different loopTimes

--- src/library/features/rotate.js
+++ src/library/features/rotate.js
@@ -7,48 +7,62 @@ class Rotate {
 
     // OPTIONAL CONFIG
     this.maxRounds = config.maxRounds || 0; // 0 === infinite
-    this.loopTime = config.loopTime || 2500;
     this.autoRotate = config.autoRotate || false;
+    this.loopTime = config.loopTime || [2500];
+    this.delay = config.delay || 600;
 
     // LOOP CONFIG
     this.IDX = -1;
     this.round = 0;
-
-    // INIT ROTATE
-    this.init();
   }
 
   init() {
-    // INIT LOOP
-    this.loop = setInterval(this.interval.bind(this), this.loopTime);
+    // DEFINE AUTOROTATE IF MAXROUNDS DEFINED
+    if (this.maxRounds > 0) this.autoRotate = true;
 
-    // EVENT LISTENERS
+    // INIT EVENT LISTENERS
     // BETTER DETECT IF TOUCH DEVICE
     this.triggers.forEach((item, index) => {
       // MOUSEOVER
-      item.addEventListener("mouseover", () => {
-        clearInterval(this.loop);
-        this.IDX = index;
-        this.update();
-      });
+      item.addEventListener(
+        "mouseenter",
+        (event) => {
+          clearTimeout(this.loop);
+
+          this.IDX = index;
+          this.autoRotate = false;
+          this.update();
+        },
+        false
+      );
 
       // MOUSEOUT
-      item.addEventListener("mouseout", () => {
-        this.loop = setInterval(this.interval.bind(this), this.loopTime);
-      });
+      item.addEventListener(
+        "mouseout",
+        (event) => {
+          // PREVENT BUBBLING
+          const e = event.toElement || event.relatedTarget;
+          if (e.parentNode == item || e == item) return;
+
+          this.autoRotate = true;
+          this.update();
+        },
+        false
+      );
     });
+
+    // INIT LOOP
+    setTimeout(() => this.interval(), this.delay);
   }
 
+  // MODIFY IDX VALUE
   interval() {
     this.IDX++;
 
     if (this.IDX === this.triggers.length) {
       this.round++;
       if (this.maxRounds === 0 || this.round < this.maxRounds) this.IDX = 0;
-      else {
-        this.reset();
-        return clearInterval(this.loop);
-      }
+      else return this.reset();
     }
 
     this.update();
@@ -58,6 +72,14 @@ class Rotate {
     this.reset();
     this.triggers[this.IDX].classList.add("active");
     this.action(this.triggers[this.IDX], this.target);
+
+    // INIT NEXT INTERVAL
+    if (!this.autoRotate) return;
+    const loopTime = this.loopTime[this.IDX]
+      ? this.loopTime[this.IDX]
+      : this.loopTime[0];
+
+    this.loop = setTimeout(() => this.interval(), loopTime);
   }
 
   reset() {
