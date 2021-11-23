5200832220dce794bd1aabd636ad4130596c8840
bminusg <info@bminusg.de>
2021-11-23 12:43:02 +0100

    Adding Expand Feature

---
+++ src/library/features/Expand.js
@@ -0,0 +1,64 @@
+class Expand {
+  constructor(options = {}) {
+    this.frameID = options.frameID;
+    this.listenerElement =
+      options.listenerElement || document.querySelector("body");
+
+    // COLLAPSE OPTIONS
+    this.collapseWidth =
+      options.collapseWidth || this.listenerElement.clientWidth + "px";
+    this.collapseHeight =
+      options.collapseHeight || this.listenerElement.offsetHeight + "px";
+    this.collapseTrigger = options.collapseTrigger || "mouseout";
+
+    // EXPAND OPTIONS
+    this.expandedWidth = options.expandedWidth || this.collapseWidth;
+    this.expandedHeight = options.expandedHeight || this.collapseHeight;
+    this.expandTrigger = options.expandTrigger || "mouseover";
+  }
+
+  init() {
+    this.listenerElement.addEventListener(this.expandTrigger, (event) => {
+      event.preventDefault();
+      this.expandAd();
+    });
+
+    this.listenerElement.addEventListener(this.collapseTrigger, (event) => {
+      event.preventDefault();
+      this.collapseAd();
+    });
+  }
+
+  expandAd() {
+    window.top.postMessage(
+      "expandAd:;:" +
+        this.frameID +
+        ":;:" +
+        this.expandedWidth +
+        ":;:" +
+        this.expandedHeight,
+      "*"
+    );
+  }
+
+  collapseAd() {
+    const expandedDirection = null;
+    const clipValue = null;
+
+    window.top.postMessage(
+      "contractAd:;:" +
+        this.frameID +
+        ":;:" +
+        this.expandedWidth +
+        ":;:" +
+        this.expandedHeight +
+        ":;:" +
+        expandedDirection +
+        ":;:" +
+        clipValue,
+      "*"
+    );
+  }
+}
+
+export default Expand;
