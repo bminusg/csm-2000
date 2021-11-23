5b62917e0986507be7cf03dbefdaf32e0ab3df44
bminusg <info@bminusg.de>
2021-11-23 12:43:45 +0100

    Setting defaultAction and click controlling

--- src/library/features/rotate.js
+++ src/library/features/rotate.js
@@ -3,8 +3,8 @@ class Rotate {
     // REQUIRED CONFIG
     this.triggerClassName = config.triggers;
     this.triggers = document.querySelectorAll(this.triggerClassName);
-    this.action = config.action;
-    this.target = config.target || document.querySelector(".creative");
+    this.action = config.action || this.defaultAction;
+    this.target = config.target || "body";
 
     // OPTIONAL CONFIG
     this.maxRounds = config.maxRounds || 0; // 0 === infinite
@@ -12,6 +12,11 @@ class Rotate {
     this.loopTime = config.loopTime || [2500];
     this.delay = config.delay || 600;
     this.datasets = config.datasets || [];
+    this.mouseoverEvent =
+      config.mouseoverEvent === undefined ? true : config.mouseoverEvent;
+
+    // CLICK CONTROLLER
+    this.clickControllers = config.clickControllers || "";
 
     // LOOP CONFIG
     this.IDX = -1;
@@ -25,11 +30,78 @@ class Rotate {
         "Can't find any markup elements for " + this.triggerClassName + ""
       );
 
+    // DEFINE TARGET
+    this.target = document.querySelector(this.target);
+
     // DEFINE AUTOROTATE IF MAXROUNDS DEFINED
     if (this.maxRounds > 0) this.autoRotate = true;
 
+    // INIT CLICK CONTROL
+    if (!this.clickControllers == "") this.appendControllerEvents();
+
     // INIT EVENT LISTENERS
-    // BETTER DETECT IF TOUCH DEVICE
+    if (this.mouseoverEvent) this.appendMouseoverEvent();
+
+    // INIT LOOP
+    setTimeout(() => this.interval(), this.delay);
+  }
+
+  // MODIFY IDX VALUE
+  interval(direction = "next") {
+    if (direction === "next") this.IDX++;
+    else this.IDX--;
+
+    if (this.IDX === this.triggers.length) {
+      this.round++;
+      if (this.maxRounds === 0 || this.round < this.maxRounds) this.IDX = 0;
+      else return this.reset();
+    }
+
+    if (this.IDX < 0) {
+      this.IDX = this.triggers.length - 1;
+    }
+
+    this.update();
+  }
+
+  defaultAction(trigger, target) {
+    const dataset = { ...trigger.dataset };
+
+    for (const data in dataset) {
+      const value = dataset[data];
+      target.dataset.stage = value;
+      return;
+    }
+  }
+
+  update() {
+    this.reset();
+
+    this.triggers[this.IDX].classList.add("is--active");
+    this.action(this.triggers[this.IDX], this.target);
+
+    if (!this.autoRotate) return;
+
+    // INIT NEXT INTERVAL
+    const loopTime = this.loopTime[this.IDX]
+      ? this.loopTime[this.IDX]
+      : this.loopTime[0];
+
+    this.loop = setTimeout(() => this.interval(), loopTime);
+  }
+
+  reset() {
+    this.triggers.forEach((item) => item.classList.remove("is--active"));
+
+    // RESET DATASETS
+    this.datasets.forEach((data) => {
+      if (!data) return;
+
+      this.target.dataset[data] = "";
+    });
+  }
+
+  appendMouseoverEvent() {
     this.triggers.forEach((item, index) => {
       // MOUSEOVER
       item.addEventListener(
@@ -48,7 +120,6 @@ class Rotate {
       item.addEventListener(
         "mouseout",
         (event) => {
-          // PREVENT BUBBLING
           const e = event.toElement || event.relatedTarget;
           if (!e || e.parentNode == item || e == item) return;
 
@@ -58,46 +129,29 @@ class Rotate {
         false
       );
     });
-
-    // INIT LOOP
-    setTimeout(() => this.interval(), this.delay);
   }
 
-  // MODIFY IDX VALUE
-  interval() {
-    this.IDX++;
+  appendControllerEvents() {
+    const controllerElems = document.querySelectorAll(this.clickControllers);
 
-    if (this.IDX === this.triggers.length) {
-      this.round++;
-      if (this.maxRounds === 0 || this.round < this.maxRounds) this.IDX = 0;
-      else return this.reset();
-    }
+    controllerElems.forEach((controller, idx) => {
+      controller.addEventListener("click", (event) => {
+        event.preventDefault();
 
-    this.update();
-  }
+        // DEFINE DIRECTION
+        let direction = idx === 0 ? "previous" : "next";
+        if (controller.dataset.direction)
+          direction = controller.dataset.direction;
 
-  update() {
-    this.reset();
-    this.triggers[this.IDX].classList.add("is--active");
-    this.action(this.triggers[this.IDX], this.target);
+        // RESET AUTOROTATE
+        this.autoRotate = false;
 
-    // INIT NEXT INTERVAL
-    if (!this.autoRotate) return;
-    const loopTime = this.loopTime[this.IDX]
-      ? this.loopTime[this.IDX]
-      : this.loopTime[0];
+        // CLEAR LOOP
+        clearTimeout(this.loop);
 
-    this.loop = setTimeout(() => this.interval(), loopTime);
-  }
-
-  reset() {
-    this.triggers.forEach((item) => item.classList.remove("is--active"));
-
-    // RESET DATASETS
-    this.datasets.forEach((data) => {
-      if (!data) return;
-
-      this.target.dataset[data] = "";
+        // CALCULATE IDX
+        this.interval(direction);
+      });
     });
   }
 }
