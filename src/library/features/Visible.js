953d260267dfc6cf12c8b9bccc1ea26fe583d21e
Benny Gebauer <bgebauer@headlessads.com>
2024-04-03 10:41:43 +0200

    adding debouncer to OKR visible message listener

--- src/library/features/Visible.js
+++ src/library/features/Visible.js
@@ -1,3 +1,5 @@
+import debounce from "../utils/debounce";
+
 class Visible {
   constructor(config = {}) {
     this.horizon = config.horizon || 0.5;
@@ -8,23 +10,29 @@ class Visible {
     if (window.top === window) return window.Creative?.startAnimation();
 
     // MESSAGE LISTENERS
-    window.addEventListener("message", (event) => {
-      const data = event.data;
-      const dataStringValuePair =
-        typeof data === "string" ? data.split("=") : [];
+    window.addEventListener("message", this.debounceCheck());
+  }
 
-      if (dataStringValuePair[0] === "isVisible") {
-        const value = parseFloat(dataStringValuePair[1]);
+  debounceCheck() {
+    return debounce(this.check.bind(this), 300); // Adjust the delay as needed (200ms in this example)
+  }
 
-        if (value > this.horizon) {
-          if (creative.isTweening) return;
-          creative.startAnimation();
-        } else {
-          if (!creative.isTweening) return;
-          creative.resetAnimation();
-        }
+  check(event) {
+    const creative = window.Creative;
+    const data = event.data;
+    const dataStringValuePair = typeof data === "string" ? data.split("=") : [];
+
+    if (dataStringValuePair[0] === "isVisible") {
+      const value = parseFloat(dataStringValuePair[1]);
+
+      if (value >= this.horizon) {
+        if (creative.isTweening) return;
+        creative.startAnimation();
+      } else {
+        if (!creative.isTweening) return;
+        creative.resetAnimation();
       }
-    });
+    }
   }
 }
 
